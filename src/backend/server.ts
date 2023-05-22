b1cd9c53a2d6c4ebd3c1125554281b93577de824
Paavo Huhtala <paavo.huhtala@gmail.com>
2023-05-22 17:07:42 +0300

    Add basic auth to backend

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -2,14 +2,33 @@ import bodyParser from "body-parser";
 import express from "express";
 import { expenseGroupApi } from "./expenseGroupApi.js";
 import { membersApi } from "./memberApi.js";
+import basicAuth from "express-basic-auth";
 
 const port = process.env.BACKEND_PORT ?? 3001;
 
-const server = express();
+const basicAuthUser = process.env.BASIC_AUTH_USER;
+const basicAuthPassword = process.env.BASIC_AUTH_PASSWORD;
 
+const server = express();
 server.use(bodyParser.json());
 
-server.get("/", (_, res) => {
+if (basicAuthUser && basicAuthPassword) {
+  console.log(`Basic auth enabled for user ${basicAuthUser}`);
+  server.use(
+    basicAuth({
+      users: { [basicAuthUser]: basicAuthPassword },
+      challenge: Boolean(basicAuthUser && basicAuthPassword),
+    }),
+  );
+} else {
+  if (process.env.NODE_ENV === "production") {
+    console.warn(
+      "Basic auth is not enabled. Please set BASIC_AUTH_USER and BASIC_AUTH_PASSWORD environment variables.",
+    );
+  }
+}
+
+server.get("/api/health", (_, res) => {
   res.send("OK");
 });
 
