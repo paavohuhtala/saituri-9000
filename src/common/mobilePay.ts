49f2c0c6a9ecf1807f08223dae1cab1485e2c53d
Paavo Huhtala <paavo.huhtala@gmail.com>
2023-06-04 17:09:53 +0300

    Convert phone numbers from international format for both types on MobilePay links

--- src/common/mobilePay.ts
+++ src/common/mobilePay.ts
@@ -1,13 +1,17 @@
 import { centsToFloatEur } from "./money";
 
 export function generateMobilePayAppLink({ phone, amount }: { phone: string; amount: number }) {
-  const formattedPhone = phone.replace(/\s/g, "");
+  const formattedPhone = transformPhoneNumber(phone);
   const formattedAmount = (centsToFloatEur(amount) ?? 0).toFixed(2);
   return `mobilepayfi://send?phone=${formattedPhone}&amount=${formattedAmount}`;
 }
 
 export function generateMobilePayWebLink({ phone, amount }: { phone: string; amount: number }) {
-  const formattedPhone = phone.replace(/\s/g, "").replace("+358", "0");
+  const formattedPhone = transformPhoneNumber(phone);
   const formattedAmount = (centsToFloatEur(amount) ?? 0).toFixed(2);
   return `https://www.mobilepay.fi/Yrityksille/Maksulinkki/maksulinkki-vastaus?phone=${formattedPhone}&amount=${formattedAmount}`;
+}
+
+function transformPhoneNumber(phone: string) {
+  return phone.replace(/\s/g, "").replace("+358", "0");
 }
