026ac8216e16e36ec4d78c98558e0f1549ff9aac
Paavo Huhtala <paavo.huhtala@gmail.com>
2023-06-04 14:45:03 +0300

    Remove unused and not working top nav button

--- src/frontend/BasePage.tsx
+++ src/frontend/BasePage.tsx
@@ -49,22 +49,6 @@ const NavContainer = styled.nav`
   border-bottom: 1px solid ${gray.x700};
 `;
 
-const NavButtons = styled.ul`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  list-style: none;
-  gap: 16px;
-`;
-
-const NavButton = styled.li`
-  font-size: 16px;
-  font-weight: 500;
-  line-height: 24px;
-  cursor: pointer;
-`;
-
 const Logo = styled(StyledLink)`
   font-size: 24px;
   font-weight: 900;
@@ -117,9 +101,6 @@ export function Layout() {
           <span>Saituri&nbsp;</span>
           <span>9000</span>
         </Logo>
-        <NavButtons>
-          <NavButton>Etusivu</NavButton>
-        </NavButtons>
       </NavContainer>
       <MainContent>
         <Outlet />
