11a53662c54e1f845e16804eea1a3c58bb971c4a
Paavo Huhtala <paavo.huhtala@gmail.com>
2023-06-19 16:00:25 +0300

    Update docker-compose -> docker compose

--- e2e-tests/test-ci.sh
+++ e2e-tests/test-ci.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Check Docker Compose version (at least 2)
-if ! docker-compose version --short | grep -E '^2\.'; then
+if ! docker compose version --short | grep -E '^2\.'; then
   echo "Docker Compose version must be at least 2"
   exit 1
 fi
