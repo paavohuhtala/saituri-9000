218857fff1a5b4948b374ff3222d81dc0bbf5604
Paavo Huhtala <paavo.huhtala@gmail.com>
2023-06-21 00:26:28 +0300

    Remove Docker version check from test scripts
    docker compose (without the dash) is already exclusive to 2.X+

--- e2e-tests/test-ui.sh
+++ e2e-tests/test-ui.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# Check Docker Compose version (at least 2)
-if ! docker compose version --short | grep -E '^2\.'; then
-  echo "Docker Compose version must be at least 2"
-  exit 1
-fi
-
 # Start Docker
 yarn db:test:start
 
