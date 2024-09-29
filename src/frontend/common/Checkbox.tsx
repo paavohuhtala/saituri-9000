6067e0ba3225f8aa91a5084bb9e9e3d55f8f4c0e
Paavo Huhtala <paavo.huhtala@gmail.com>
2024-09-29 20:11:16 +0300

    Fix code because Biome broke it

--- src/frontend/common/Checkbox.tsx
+++ src/frontend/common/Checkbox.tsx
@@ -1,5 +1,5 @@
+import React from "react";
 import { IconCheck } from "@tabler/icons-react";
-import type React from "react";
 import styled from "styled-components";
 import { gray } from "../theme";
 
